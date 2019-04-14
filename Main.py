47720d9efc9f9a248c2620adcdd3511453826899
TheCesebHD <49492360+TheCesebHD@users.noreply.github.com>
2019-04-14 18:04:44 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,11 +8,7 @@ import HandleComment
 REDDIT_USERNAME = 'BuggyMemeBot'
 
 # login data
-reddit = praw.Reddit(client_id='4aK_5w6Jenjlpw',
-                     client_secret='n-HoTqZdcNMwdmBuD8bWCwT0dWc',
-                     username=REDDIT_USERNAME,
-                     password='bugbot2018',
-                     user_agent='a cool bot by /u/kleecarim')
+reddit = praw.Reddit(censured)
 
 subreddit = reddit.subreddit('khazixmainstest')
 
